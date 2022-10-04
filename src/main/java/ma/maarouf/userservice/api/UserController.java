fbd105416655c842e34f3f9b5e59aa0e43c9f6d0
omaarouf <Ouassime.MAAROUF@um6p.ma>
2022-10-04 02:58:19 +0100

    final commit

--- src/main/java/ma/maarouf/userservice/api/UserController.java
+++ src/main/java/ma/maarouf/userservice/api/UserController.java
@@ -1,16 +1,30 @@
 package ma.maarouf.userservice.api;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import ma.maarouf.userservice.domain.AppRole;
 import ma.maarouf.userservice.domain.AppUser;
 import ma.maarouf.userservice.service.AppUserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.net.URI;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @RestController @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -39,6 +53,38 @@ public class UserController {
         userService.addRoleToUser(form.getUsername(), form.getRoleName());
         return ResponseEntity.ok().build();
     }
+    @GetMapping("/token/refresh")
+    public void refreshTokens(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refresh_token = authorizationHeader.substring("Bearer ".length());
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier = JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refresh_token);
+                String username = decodedJWT.getSubject();
+                AppUser user = userService.getUser(username);
+                String access_token = JWT.create()
+                        .withSubject(user.getUsername())
+                        .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)) // 10 minutes
+                        .withIssuer(request.getRequestURL().toString())
+                        .withClaim("roles",user.getRoles().stream().map(AppRole::getName).collect(Collectors.toList()))
+                        .sign(algorithm);
+                Map<String ,String> tokens = Map.of("access_token", access_token, "refresh_token", refresh_token);
+                response.setContentType("application/json");
+                new com.fasterxml.jackson.databind.ObjectMapper().writeValue(response.getOutputStream(), tokens);
+            } catch (Exception e) {
+                response.setHeader("error", e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> error = Map.of("error_message", e.getMessage());
+                response.setContentType("application/json");
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+            }
+        }else {
+            throw new RuntimeException("Refresh token is missing");
+        }
+    }
+
 
     @Data
     class RoleToUserForm {
